720399c771522443e784981df1b5d3bde1d04c26
IamVDD <vodinhdat03@gmail.com>
2023-06-17 08:21:21 +0700

    ok

--- ENV/model/agent.py
+++ ENV/model/agent.py
@@ -30,7 +30,8 @@ class Agent(RolloutBuffer):
             - per: (`List`): per file
         """
         with torch.no_grad():
-            policy,value = self.model(torch.tensor(state.reshape(1,1,-1),dtype=torch.float32))
+            tensor_state = torch.tensor(state.reshape(1,1,-1),dtype=torch.float32)
+            policy,value = self.model(tensor_state)
             policy       = policy.squeeze().numpy()
             list_action  = self.env.getValidActions(state)
             actions      = np.where(list_action==1)[0]
@@ -38,6 +39,7 @@ class Agent(RolloutBuffer):
             # print(action)
 
             if self.env.getReward(state)==-1:
+                self.model.memory.update(tensor_state)
                 self.step_current_game += 1
                 self.add_data(state        = state,
                               action       = action,
