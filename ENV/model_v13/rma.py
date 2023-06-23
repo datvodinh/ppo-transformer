7de88a4da8956e01ca52aadf8044767e6ab74c44
IamVDD <vodinhdat03@gmail.com>
2023-06-23 12:08:24 +0700

    add v13

---
+++ ENV/model_v13/rma.py
@@ -0,0 +1,155 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class RelativeMultiheadAttention(nn.Module):
+    """Calculate Relative Multihead Attention"""
+    def __init__(self,
+                 embed_dim:int,
+                 num_heads:int) -> None:
+        """Overview:
+            Init AttentionXL.
+        Arguments:
+            - embed_dim (:obj:`int`): dimension of embedding
+            - num_heads (:obj:`int`): number of heads for multihead attention
+        """
+
+        super(RelativeMultiheadAttention,self).__init__()
+        self.embed_dim  = embed_dim
+        self.d          = np.sqrt(embed_dim)
+        self.num_heads  = num_heads
+        self.heads_dim  = embed_dim // num_heads
+        assert embed_dim % num_heads == 0, "embed_dim % num_heads should be zero."
+        
+        self.Queries    = nn.Linear(embed_dim,embed_dim)
+        self.Keys       = nn.Linear(embed_dim,embed_dim)
+        self.Values     = nn.Linear(embed_dim,embed_dim)
+        self.Pos        = nn.Linear(embed_dim,embed_dim,bias=False)
+
+        self.out_projection = nn.Linear(embed_dim,embed_dim)
+
+    def forward(self,
+                query: torch.Tensor,
+                key: torch.Tensor,
+                value: torch.Tensor,
+                pos_embedding: torch.Tensor,
+                U: torch.Tensor,
+                V: torch.Tensor,
+                mask: None,
+                padding_mask: None)->torch.Tensor:
+        """Overview:
+            Compute Relative Multi-head Attention.
+
+        Arguments:
+            - query (`torch.Tensor`): attention input of shape (cur_seq, bs, input_dim)
+            - key (`torch.Tensor`): attention input of shape (full_seq, bs, input_dim)
+            - value (`torch.Tensor`): attention input of shape (full_seq, bs, input_dim)
+            - pos_embedding (`torch.Tensor`): positional embedding of shape (full_seq, 1, full_seq)
+            - U (`torch.Tensor`): global content bias, shape (num_heads,heads_dim)
+            - V (`torch.Tensor`): global position bias, shape (num_heads,heads_dim)
+            - mask (`Optional[torch.Tensor|None]`): attention mask of shape (cur_seq, full_seq, 1)
+            - padding_mask (`Optional[torch.Tensor|None]`): attention mask of shape (bs,cur_seq, full_seq)
+            - full_seq = prev_seq + cur_seq
+
+        Returns:
+            - alpha (`torch.Tensor`): attention output of shape (cur_seq, bs, input_dim)
+        """
+
+        batch_size = query.shape[1]
+        query_len,key_len,value_len = query.shape[0], key.shape[0], value.shape[0]
+        
+        queries = self.Queries(query).view(query_len,batch_size,self.num_heads,self.heads_dim)
+        keys    = self.Keys(key).view(key_len,batch_size,self.num_heads,self.heads_dim)
+        values  = self.Values(value).view(value_len,batch_size,self.num_heads,self.heads_dim)
+        R       = self.Pos(pos_embedding).view(-1,self.num_heads,self.heads_dim)
+
+        content_score   = torch.einsum("qbhd,kbhd->qkbh",[queries+U,keys])
+        position_score  = torch.einsum("qbhd,khd->qkbh",[queries+V,R])
+        position_score  = self._rel_shift(position_score)
+        attention_score = (content_score + position_score) / self.d # (query_len,key_len,batch_size,num_heads)
+
+        assert torch.isnan(queries+U).any()==False,f"{queries+U}{queries}{U}"
+        assert torch.isnan(keys).any()==False,f"{keys}"
+        assert torch.isnan(content_score).any()==False, "content score return NaN!" 
+        assert torch.isnan(position_score).any()==False, "position score return NaN!" 
+        assert torch.isnan(attention_score).any()==False, "attention score return NaN!" 
+
+        if mask is not None:
+            mask = mask.unsqueeze(-1)  #  cur_seq x full_seq x 1 x 1
+            assert mask.shape[:2] == attention_score.shape[:2] , f"Mask shape: {mask.shape} vs Attention shape: {attention_score.shape}" # check shape of mask
+            attention_score = attention_score.masked_fill(mask,float("-1e20")).type_as(attention_score)
+        
+        if padding_mask is not None:
+            padding_mask = self._padding_mask(padding_mask,query_len,key_len) # (batch_size,cur_seq, full_seq)
+            padding_mask = padding_mask.permute(1,2,0).unsqueeze(-1) # (cur_seq, full_seq,batch_size,1)
+            assert padding_mask.shape[:2] == attention_score.shape[:2], f"Mask shape: {padding_mask.shape} vs Attention shape: {attention_score.shape}"  # check shape of padding_mask
+            attention_score = attention_score.masked_fill(padding_mask,float("-1e20")).type_as(attention_score)
+            
+        attention_score = torch.softmax(attention_score,dim=1)
+
+        assert torch.isnan(attention_score).any()==False, "attention score return NaN after softmax!" 
+
+        alpha = torch.einsum("qkbh,vbhd->qbhd",[attention_score,values])
+        alpha = alpha.contiguous().view(alpha.size(0),alpha.size(1),self.embed_dim) # alpha shape: (query_len,batch_size,embed_dim)
+
+        assert torch.isnan(alpha).any()==False, "RMA alpha return NaN!" 
+
+        return self.out_projection(alpha)
+    
+
+    def _rel_shift(self, x: torch.Tensor, zero_triu=False):
+        """
+        Overview:
+            Relatively shift the attention score matrix.
+
+        Example:
+            a00 a01 a02      0 a00 a01 a02       0  a00 a01      a02  0  a10     a02  0   0
+            a10 a11 a12  =>  0 a10 a11 a12  =>  a02  0  a10  =>  a11 a12  0  =>  a11 a12  0
+            a20 a21 a22      0 a20 a21 a22      a11 a12  0       a20 a21 a22     a20 a21 a22
+
+        Arguments:
+            - x (`torch.Tensor`): input tensor of shape (cur_seq, full_seq, bs, head_num).
+            - zero_upper (`bool`): if True set the upper-right triangle to zero.
+            
+        Returns:
+            - x (`torch.Tensor`): input after relative shift. Shape (cur_seq, full_seq, bs, head_num).
+        """
+        zero_pad = torch.zeros((x.size(0), 1, *x.size()[2:]), dtype=x.dtype)
+        x_padded = torch.cat([zero_pad, x], dim=1)
+
+        x_padded = x_padded.view(x.size(1) + 1, x.size(0), *x.size()[2:])
+
+        x = x_padded[1:].view_as(x)
+
+        if zero_triu:
+            ones = torch.ones((x.size(0), x.size(1)))
+            x = x * torch.tril(ones, x.size(1) - x.size(0))[:,:,None,None]
+
+        return x
+    
+    def _padding_mask(self,pad,seq_len,full_len):
+        """
+        Create a padding mask for attention based on the time step array.
+
+        Args:
+            pad (torch.Tensor): Time step array tensor of shape (batch_size, cur_seq),
+                where 1 represents a real time step and 0 represents padding.
+
+        Returns:
+            torch.Tensor: Padding mask tensor of shape (batch_size,cur_seq, full_seq),
+                where 0 indicates padding positions and 1 indicates real time steps.
+        """
+
+        pad      = pad.unsqueeze(2) # (batch_size, max_episode_step,1)
+
+        pad_mask = pad @ pad.permute(0,2,1) # (batch_size, max_episode_step,max_episode_step)
+        if full_len > seq_len:
+            pad_mem = torch.ones(pad.shape[0],pad.shape[1],full_len - seq_len)
+            return torch.cat([pad_mem,pad_mask],dim = 2) == 0
+        else:
+            return pad_mask == 0
+
+
+
+
---
+++ ENV/model_v13/trainer.py
@@ -0,0 +1,142 @@
+from torch.distributions import Categorical,kl_divergence
+import torch
+import torch.nn as nn
+import numpy as np
+import time
+import os
+
+from setup import make
+from model_v13.model import PPOTransformerModel
+from model_v13.agent import Agent
+from model_v13.writer import Writer
+
+class Trainer:
+    """Train the model"""
+    def __init__(self,config,game_name,writer_path=None) -> None:
+        self.config        = config
+        self.env           = make(game_name)
+
+        self.model         = PPOTransformerModel(config,self.env.getStateSize(),self.env.getActionSize())
+        self.optimizer     = torch.optim.AdamW(self.model.parameters(),lr=config['lr'])
+        self.memory_length = config["memory_length"]
+        if writer_path is not None:
+            self.writer    = Writer(writer_path)
+        self.agent         = Agent(self.env,self.model,config)
+    
+    def _cal_loss(self,value,value_new,entropy,log_prob,log_prob_new,advantage):
+        """
+        Overview:
+            Calculate Total Loss
+
+        Arguments:
+            - value: (`torch.Tensor`):  
+            - value_new: (`torch.Tensor`): 
+            - entropy: (`torch.Tensor`): 
+            - log_prob: (`torch.Tensor`): 
+            - log_prob_new: (`torch.Tensor`): 
+            - advantage: (`torch.Tensor`):  
+
+        Return:
+            - actor_loss: (`torch.Tensor`): 
+            - critic_loss: (`torch.Tensor`): 
+            - total_loss: (`torch.Tensor`): 
+
+        
+        """
+        #Calculate returns and advantage
+        returns         = value + advantage
+        advantage       = (advantage - advantage.mean()) / (advantage.std() + 1e-6)
+        #Ratios and KL divergence
+        ratios          = torch.exp(torch.clamp(log_prob_new-log_prob.detach(),min=-20.,max=5.))
+        Kl              = kl_divergence(Categorical(logits=log_prob), Categorical(logits=log_prob_new))
+        #PG loss
+        R_dot_A = ratios * advantage
+        actor_loss      = -torch.where(
+                            (Kl >= self.config["policy_kl_range"]) & (R_dot_A > advantage),
+                            R_dot_A - self.config["policy_params"] * Kl,
+                            R_dot_A
+                        ).mean()
+        #Critic loss
+        value_clipped   = value + torch.clamp(value_new - value, -self.config["value_clip"], self.config["value_clip"])
+        critic_loss     = 0.5 * torch.max((returns-value_new)**2,(returns-value_clipped)**2).mean()
+        #Total loss
+        total_loss      = actor_loss + self.config["critic_coef"] * critic_loss - self.config["entropy_coef"] * entropy.mean()
+        # print(actor_loss,critic_loss,total_loss)
+        return actor_loss, critic_loss, total_loss, entropy.mean()
+    
+    def _remove_padding(self,
+                 t:torch.Tensor,
+                 padding:torch.Tensor,
+                 value = -100):
+        """
+        Overviews:
+            Return tensor with padding for policy and value loss.
+        Arguments:
+            - t: (`torch.Tensor`): tensor
+            - padding: (`torch.Tensor`): padding mask
+        Returns:
+            - Tensor t without padding
+        """
+        t =  t.masked_fill(padding==0,value=float(str(value)))
+        return t[t!=value]
+
+    
+
+
+    def train(self,write_data=True):
+        training = True
+
+        step = 0
+        while training:
+
+            win_rate = self.agent.run(num_games=self.config["num_game_per_batch"])
+            self.agent.rollout.cal_advantages(self.config["gamma"],self.config["gae_lambda"])
+            
+            self.model.train()
+            
+            for _ in range(self.config["num_epochs"]):
+                mini_batch_loader   = self.agent.rollout.mini_batch_loader(self.config)
+                for mini_batch in mini_batch_loader:
+                    pol_new,val_new = self.model(mini_batch["states"],mini_batch["padding"])
+                    val_new         = val_new.squeeze(1)
+                    # print(pol_new, mini_batch["action_mask"])
+                    B,M,A = mini_batch["action_mask"].shape
+                    # print(pol_new.shape,(B,M,A))
+                    categorical_new = Categorical(logits=pol_new.masked_fill(mini_batch["action_mask"].view(B*M,A)==0,float('-1e20')))
+                    log_prob_new    = categorical_new.log_prob(mini_batch["actions"].view(1,-1)).squeeze(0)
+                    entropy         = categorical_new.entropy()
+
+                    padding = mini_batch["padding"].reshape(-1)
+                    actor_loss, critic_loss, total_loss,entropy = self._cal_loss(
+                        value        = self._remove_padding(mini_batch["values"].reshape(-1),padding).detach(),
+                        value_new    = self._remove_padding(val_new,padding),
+                        entropy      = self._remove_padding(entropy,padding),
+                        log_prob     = self._remove_padding(mini_batch["probs"].reshape(-1),padding).detach(),
+                        log_prob_new = self._remove_padding(log_prob_new,padding),
+                        advantage    = self._remove_padding(mini_batch["advantages"].reshape(-1),padding),
+                    )
+                    with torch.autograd.set_detect_anomaly(self.config["set_detect_anomaly"]):
+                        if not torch.isnan(total_loss).any():
+                            self.optimizer.zero_grad()
+                            total_loss.backward()
+                            nn.utils.clip_grad_norm_(self.model.parameters(),max_norm=self.config["max_grad_norm"])
+                            self.optimizer.step()
+                    if write_data:
+                        
+                        with torch.no_grad():
+                            self.writer.add(
+                                step        = step,
+                                win_rate    = win_rate,
+                                reward      = self.agent.rollout.batch["rewards"].mean(),
+                                entropy     = entropy,
+                                actor_loss  = actor_loss,
+                                critic_loss = critic_loss,
+                                total_loss  = total_loss
+                            )
+                            step+=1
+            
+            self.agent.rollout.reset_data()
+
+    def _save_model(model:PPOTransformerModel,path):
+        torch.save(model.state_dict(),path)
+
